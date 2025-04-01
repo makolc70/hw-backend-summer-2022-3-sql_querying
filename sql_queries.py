d98a1ace9e3897c9d3265bdf63e66bad2f3c316f
makolc70 <81307690+makolc70@users.noreply.github.com>
2025-04-01 06:46:21 -0700

    Update sql_queries.py

--- sql_queries.py
+++ sql_queries.py
@@ -2,7 +2,6 @@
 # Duration - разница между scheduled_arrival и scheduled_departure.
 # В ответе должно быть 2 колонки [flight_no, duration]
 TASK_1_QUERY = "SELECT flight_no, (scheduled_arrival - scheduled_departure) AS duration FROM flights ORDER BY duration LIMIT 5"
-"""
 #  flight_no | duration
 # -----------+----------
 #  PG0235    | 00:25:00
@@ -16,7 +15,6 @@ TASK_1_QUERY = "SELECT flight_no, (scheduled_arrival - scheduled_departure) AS d
 # количество упоминаний которых меньше 50
 # В ответе должно быть 2 колонки [flight_no, count]
 TASK_2_QUERY = "SELECT flight_no, COUNT(*) AS count FROM flights GROUP BY flight_no HAVING COUNT(*) < 50 ORDER BY count DESC LIMIT 3"
-"""
 #  flight_no | count
 # -----------+-------
 #  PG0260    |    27
@@ -26,7 +24,6 @@ TASK_2_QUERY = "SELECT flight_no, COUNT(*) AS count FROM flights GROUP BY flight
 # Вывести число перелетов внутри одной таймзоны
 # Нужно вывести 1 значение в колонке count
 TASK_3_QUERY = "SELECT COUNT(*) AS count FROM flights WHERE origin_timezone = destination_timezone"
-"""
 #  count
 # --------
 #  16824
